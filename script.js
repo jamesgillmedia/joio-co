9b7ec4f8e024d0c34d9d9cffa349c8a1f758c0be
jamesgillmedia <123349560+jamesgillmedia@users.noreply.github.com>
2023-05-11 18:16:49 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const title = document.getElementById('title');
+title.addEventListener('mouseover', function() {
+  this.textContent = 'JOIO CO';
+});
+title.addEventListener('mouseout', function() {
+  this.textContent = 'joio.co';
+});
