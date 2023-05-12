cd9fc99f9f9f83f51a670b66b84fc39498480341
jamesgillmedia <123349560+jamesgillmedia@users.noreply.github.com>
2023-05-12 12:27:12 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const title = document.getElementById('title');
-title.addEventListener('mouseover', function() {
-  this.textContent = 'JOIO CO';
-});
-title.addEventListener('mouseout', function() {
-  this.textContent = 'joio.co';
-<script src="script.js"></script>
-});
