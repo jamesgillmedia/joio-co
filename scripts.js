8ac1fe53d4b7c202a7e0096d7d521064e80db41c
jamesgillmedia <123349560+jamesgillmedia@users.noreply.github.com>
2023-05-12 12:49:15 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const title = document.getElementById('title');
+title.addEventListener('mouseover', function() {
+  this.textContent = 'JOIO CO';
+});
+title.addEventListener('mouseout', function() {
+  this.textContent = 'joio.co';
+});
+
+const footerTitle = document.getElementById('footer-title');
+footerTitle.addEventListener('mouseover', function() {
+  this.textContent = 'JOIO CO';
+});
+footerTitle.addEventListener('mouseout', function() {
+  this.textContent = 'joio.co';
+});
