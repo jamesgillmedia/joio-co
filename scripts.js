ec83673088c7b0db142e0aef98d497a70c9b3f0f
jamesgillmedia <123349560+jamesgillmedia@users.noreply.github.com>
2024-02-16 19:21:28 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggle = document.getElementById('dark-mode-toggle');
-    toggle.addEventListener('click', function() {
-        const currentTheme = document.body.getAttribute('data-theme');
-        if (currentTheme === 'dark') {
-            document.body.setAttribute('data-theme', 'light');
-        } else {
-            document.body.setAttribute('data-theme', 'dark');
-        }
-    });
-});
 
 
 <script>
